c6008dd7277bc486b2d90f452e6b26bef990a147
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2026-04-07 03:27:26 -0700

    paskages-install-setup-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function App() {
   return (
-    <div className="bg-blue-500 text-white text-2xl p-8">
-      Tailwind is working
+    <div className="p-8">
+      <Button>SkillNet works</Button>
     </div>
   )
 }
