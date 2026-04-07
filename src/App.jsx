fd47a4c8904395d662b5960dd99a61fc61076383
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2026-04-07 03:39:43 -0700

    Routing-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Catalog from './pages/Catalog'
+import SkillDetail from './pages/SkillDetail'
+import ListSkill from './pages/ListSkill'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <div className="p-8">
-      <Button>SkillNet works</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/skills/:id" element={<SkillDetail />} />
+        <Route path="/list-skill" element={<ListSkill />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
