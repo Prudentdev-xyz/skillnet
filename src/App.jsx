0a4dec1fcf8b893e6aac9f88aec88cb7c634c550
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2026-04-09 02:04:25 -0700

    Route-Navbar&Footer-update-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Catalog from './pages/Catalog'
-import SkillDetail from './pages/SkillDetail'
-import ListSkill from './pages/ListSkill'
-import Dashboard from './pages/Dashboard'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import SkillDetail from "./pages/SkillDetail";
+import ListSkill from "./pages/ListSkill";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/skills/:id" element={<SkillDetail />} />
-        <Route path="/list-skill" element={<ListSkill />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/skills/:id" element={<SkillDetail />} />
+            <Route path="/list-skill" element={<ListSkill />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
